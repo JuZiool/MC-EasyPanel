550fa0a77ebf9ee840e2ba361d45310b5eab96c2
JuZiool <juziool@users.noreply.github.com>
2026-06-22 07:50:16 +0800

    全局覆写console方法，为终端输出添加[HH:mm:ss]时间戳

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,5 +1,22 @@
 import winston from 'winston'
 
+// 全局覆写 console 方法，为所有控制台输出添加 [HH:mm:ss] 时间戳
+const pad2 = (n: number) => n.toString().padStart(2, '0')
+const timestamp = () => {
+  const now = new Date()
+  return `[${pad2(now.getHours())}:${pad2(now.getMinutes())}:${pad2(now.getSeconds())}]`
+}
+
+const originalLog = console.log
+const originalError = console.error
+const originalWarn = console.warn
+const originalInfo = console.info
+
+console.log = (...args: any[]) => originalLog(timestamp(), ...args)
+console.error = (...args: any[]) => originalError(timestamp(), ...args)
+console.warn = (...args: any[]) => originalWarn(timestamp(), ...args)
+console.info = (...args: any[]) => originalInfo(timestamp(), ...args)
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
